63e65c7f368e86010f84e54071d171830a5cf3d0
alexAlice <alex@alice.si>
2019-08-24 16:07:39 +0200

    initial preview component implementation

--- src/clausesConfig.js
+++ src/clausesConfig.js
@@ -4,7 +4,7 @@ let conf = {
       {
         id: 'donation',
         title: 'donation',
-        legalContent: 'Some legal text alalalalalalal {max}',
+        legalContent: 'Some legal text alalalalalalal {{max}}',
         arguments: {
           max: 'number'
         },
@@ -13,7 +13,7 @@ let conf = {
       {
         id: 'comission',
         title: 'comission',
-        legalContent: 'Some other legal text alalalal {max} {address}',
+        legalContent: 'Some other legal text alalalal {{max}} {{address}}',
         arguments: {
           max: 'number',
           address: 'string',
@@ -25,7 +25,7 @@ let conf = {
       {
         id: 'defaultInvestmentClause',
         title: 'Default Investment Clause',
-        legalContent: 'Imvestemt clause ... Min: {min}',
+        legalContent: 'Imvestemt clause ... Min: {{min}}',
         arguments: {
           min: 'number',
           max: 'number',
